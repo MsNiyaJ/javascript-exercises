a0fd5428ddf41c2349401cf427027cf684c1fdfc
Shaniya <47842684+MsNiyaJ@users.noreply.github.com>
2021-08-16 14:27:15 -0400

    Add files via upload

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let celcius = (5/9) * (fahrenheit - 32);// Convert farenheit to celcius
 
+  if (celcius === 0)
+    return Number(celcius.toFixed(0));    // Round celcius to nearest whole number
+	return Number(celcius.toFixed(1));      // Round celsius to nearest tenth 
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let fahrenheit = celcius * (9/5) + 32;  // Convert celcius to farenheit
 
+  if (fahrenheit === 0)
+    return Number(fahrenheit.toFixed(0)); // Round fahrenheit to nearest whole number
+  return Number(fahrenheit.toFixed(1));   // Round fahrenheit to nearest tenth
 };
 
 module.exports = {

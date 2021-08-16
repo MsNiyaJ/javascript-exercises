6b1a7bbb0ec97a14dc78cf6e92882b31456fef2f
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-16 17:17:29 -0400

    made function sum()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function(num1, num2) {
-    
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
 const multiply = function(num1, num2) {

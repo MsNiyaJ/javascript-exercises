8d24a8d95dd33fd41e855c31af63fcab223d8f3b
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-16 18:17:05 -0400

    created palindromes() && removePunctionationAndWhitespace()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const reverseString = require("../03_reverseString/reverseString");
 
+const palindromes = function (str) {
+    str = removePunctionationAndWhitespace(str).toLowerCase();
+
+    let reversedString = reverseString(str); //Reverse the given string    
+    if(reversedString === str) return true;
+    return false;
 };
+
+//Use regex to remove punctuation and whitespace
+function removePunctionationAndWhitespace(str){
+    var punctuationless = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var finalString = punctuationless.replace(/\s/g, '');
+    return finalString;
+}
 
 module.exports = palindromes;

06f99587f8fce1df96bd87869afb0a3ed84b41ad
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-17 01:47:47 -0400

    created getTheTitles(books)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookTitles = [];    //initializing empty array
 
+    //For each book, add the title to bookTitles array
+    books.forEach(book => {
+        bookTitles.push(book['title']); 
+    });
+    return bookTitles;
 };
 
 module.exports = getTheTitles;

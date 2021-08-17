ce5ffd073da0f501ecfa0ce08926a94ab9819948
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-17 02:17:44 -0400

    Created findTheOldest(people) and getAge(person)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];   //initilize first person as the oldest
+    let oldestPersonsAge = getAge(people[0]); 
+    
+    people.forEach(person => {
+        let personsAge = getAge(person);
+        if(personsAge > oldestPersonsAge){
+            oldestPerson = person;
+            oldestPersonsAge = personsAge;
+        }
+    });
+
+return oldestPerson;
 
 };
+
+//Returns the age of a person
+function getAge(person){
+    //If the person is still alive, get their age as of today
+    if(person.yearOfDeath == undefined){
+        const d = new Date(); 
+        let ageAsOfToday = d.getFullYear() - person.yearOfBirth; //today's year - year of birth
+        return ageAsOfToday;
+    }
+    //Else determine what their age was before they died
+    let age = person.yearOfDeath - person.yearOfBirth;
+        return age;
+}
 
 module.exports = findTheOldest;
